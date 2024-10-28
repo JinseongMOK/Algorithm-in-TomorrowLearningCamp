266475cc3e1ce83706b6723e8d66c0d5e42d0311
JMOK <106071825+JinseongMOK@users.noreply.github.com>
2024-10-28 18:19:15 +0900

    Delete Day1_Kids_With_the_Greatest_Number_of_Candies.dart
    change branch

--- Day1_Kids_With_the_Greatest_Number_of_Candies.dart
+++
@@ -1,7 +0,0 @@
-class Solution {
-  List<bool> kidsWithCandies(List<int> candies, int extraCandies) {
-    int maxCandies = candies.reduce((a, b) => a > b ? a : b);
-  
-    return candies.map((candy) => candy + extraCandies >= maxCandies).toList();
-    }
-}
